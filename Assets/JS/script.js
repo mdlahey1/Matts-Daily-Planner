e9167020a9943eca6e2de95742dfe725a2d53570
Matt Lahey <mdlahey@outlook.com>
2022-10-18 21:44:15 -0400

    added a clear button that removes a task from the relevant hour block

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,7 +1,8 @@
 //Define Variables
 var currentDay = $("#currentDay");
-var timeBlock = $('.time-block');
+var timeBlock = $(".time-block");
 var saveBtn = $(".saveBtn");
+var clearBtn = $(".clearBtn");
 
 //Display the current date in the jumbotron element
 var currDate = moment().format('dddd, MMM Do YYYY');
@@ -11,13 +12,13 @@ currentDay.text(currDate);
 function timeColors() {
     //store current time in a variable
     var currentTime = moment().hour();
-    console.log(currentTime);
+    //console.log(currentTime);
 
     //loop through all time-blocks
     timeBlock.each(function() {
         //create variable that defines which time-block the loop is currently on
         var currentBlock = parseInt($(this).attr("id").split("hour")[1]);
-        console.log(currentBlock);
+        //console.log(currentBlock);
 
         //Check to see if the time-block is in the past, future or present and update the class accordingly
         if (currentTime > currentBlock) {
@@ -38,26 +39,37 @@ function timeColors() {
     });
 };
 
-//Create a function with an event listener that allows the user to save calendar tasks to individual hours when the save button is hit
-$(document).ready(function() {
-    saveBtn.on("click", function() {
-        //Create variables that will be stored in local storage
-        var task = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-
-        //Save description using local storage
-        localStorage.setItem(time, task);
-    });
-});
-
 //Create a function that retrieves existing tasks from local storage
 function renderTasks() {
     timeBlock.each(function() {
         var currentBlock = parseInt($(this).attr("id").split("hour")[1]);
-        console.log("Tasks:" + currentBlock);
         $(this).children(".description").val(localStorage.getItem("hour" + currentBlock));
     });
 }
+
+//Create a function with an event listener that allows the user to save calendar tasks for individual hours when the save button is hit
+$(document).ready(function() {
+    saveBtn.on("click", function() {
+        //Create variables that will be stored in local storage
+        var time = $(this).parent().parent().attr("id");
+        var task = $(this).parent().siblings(".description").val();
+
+        //Save the task using local storage
+        localStorage.setItem(time, task);
+    });
+});
+
+//Create a function with an event listener that allows the user to clear calendar tasks for individual hours when the clear button is hit
+$(document).ready(function() {
+    clearBtn.on("click", function() {
+        //Create a variable that stores the correct time-block id
+        var time = $(this).parent().parent().attr("id");
+        
+        //Delete the task from local storage
+        localStorage.removeItem(time);
+        location.reload();
+    });
+});
 
 timeColors();
 renderTasks();
